dd4a366b93d8450644108fc8b04bb1300c5f9a69
Matthew Olson <o.matt432@gmail.com>
2023-12-10 22:39:46 -0600

    initial project setup. tkinter, imgs, constants.

---
+++ main.py
@@ -0,0 +1,27 @@
+import tkinter
+
+LANG_FONT = ("Arial", 22, "italic")
+WORD_FONT = ("Arial", 36, "bold")
+SPEECH_COMPONENT_FONT = ("Arial", 18, "normal")
+BG_COLOR = "#d9d9d9"
+
+THUMB_UP_IMG = "/img/thumbup.png"
+THUMB_DOWN_IMG = "/img/thumbdown.png"
+FLASH_FRONT = "/img/flashcard_front.png"
+FLASH_BACK = "/img/flashcard_back.png"
+
+window = tkinter.Tk()
+window.title("Flashcards - Language App")
+window.config(padx=50, pady=50, bg=BG_COLOR)
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
