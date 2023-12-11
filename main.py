6d87ff7f6d0c6655f3dbf769550ddadb22fe43be
Matthew Olson <o.matt432@gmail.com>
2023-12-11 01:37:12 -0600

    add dummy text data

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import tkinter
 
-LANG_FONT = ("Arial", 22, "italic")
-WORD_FONT = ("Arial", 36, "bold")
+WINDOW_LANGS_FONT = ("Courier", 34, "bold")
+CARD_LANG_FONT = ("Arial", 22, "italic")
+CARD_WORD_FONT = ("Arial", 32, "bold")
 SPEECH_COMPONENT_FONT = ("Arial", 18, "normal")
 BG_COLOR = "#cccccc"
 
@@ -17,26 +18,26 @@ window.config(padx=20, pady=20, bg=BG_COLOR)
 
 # Flashcard Back - canvas
 canvas = tkinter.Canvas(width=700, height=400, bg=BG_COLOR, highlightthickness=0)
-flash_back = tkinter.PhotoImage(file=FLASH_BACK)
-canvas.create_image((350, 220), image=flash_back)
+fl_back_img = tkinter.PhotoImage(file=FLASH_BACK)
+flash_back = canvas.create_image((350, 220), image=fl_back_img)
+card_lang = canvas.create_text((180, 110), text="French", font=CARD_LANG_FONT)
+card_word = canvas.create_text((350, 226), text="Word", font=CARD_WORD_FONT)
+card_speech_component = canvas.create_text((350, 326), 
+                                           text="NOUN", font=SPEECH_COMPONENT_FONT)
 canvas.grid(row=1, column=0, columnspan=3)
 
 # Tkinter Top - label
-canvas.create_text((350, 30), text="English - French", font=LANG_FONT)
-
-# top_label = tkinter.Label(text="English - French", font=WORD_FONT, bg=BG_COLOR)
-# top_label.grid(row=0, column=1, pady=(0, 10))
-
-# ThumbUp - button
-thumb_up_img = tkinter.PhotoImage(file=THUMB_UP_IMG)
-thumb_up_btn = tkinter.Button(image=thumb_up_img, highlightthickness=0, bg=BG_COLOR)
-thumb_up_btn.grid(row=2, column=0, pady=(0, 20))
+top_label = canvas.create_text((350, 30), text="French - English", font=WINDOW_LANGS_FONT)
 
 # ThumbDown - button
 thumb_down_img = tkinter.PhotoImage(file=THUMB_DOWN_IMG)
 thumb_down_btn = tkinter.Button(image=thumb_down_img, highlightthickness=0, bg=BG_COLOR)
-thumb_down_btn.grid(row=2, column=2, pady=(0, 20))
+thumb_down_btn.grid(row=2, column=0, pady=(0, 20))
 
+# ThumbUp - button
+thumb_up_img = tkinter.PhotoImage(file=THUMB_UP_IMG)
+thumb_up_btn = tkinter.Button(image=thumb_up_img, highlightthickness=0, bg=BG_COLOR)
+thumb_up_btn.grid(row=2, column=2, pady=(0, 20))
 
 
 
